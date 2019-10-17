64f69e855e0570af9ee53d5004a1e32a725c7278
MouseMary <judjnet@yandex.ru>
2019-10-17 16:24:23 +0300

    working  tests

--- migrations/1571249852_test_super_contract.js
+++ migrations/1571249852_test_super_contract.js
@@ -2,7 +2,7 @@ const Supercontract = artifacts.require("Supercontract");
 const TestSupercontract = artifacts.require("TestSupercontract");
 
 module.exports = function(_deployer) {
-  _deployer.link(Supercontract, TestSupercontract);
-  _deployer.deploy(TestSupercontract);
+ // _deployer.link(Supercontract, TestSupercontract);
+ // _deployer.deploy(TestSupercontract);
   // Use deployer to state migration tasks.
 };
